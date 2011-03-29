ba3910203ccd00560586d0ff3df92005b78ca217
ilake <lake.ilakela+github@gmail.com>
2011-03-29 21:13:40 +0800

    mail to setting use user name

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,25 +2,30 @@ class Notifier < ActionMailer::Base
 
   def registration_confirmation(user)
     @account = user
-    mail(:to => user.email, :subject => "Registered")
+    mail(:to => named_email(user), :subject => "Registered")
   end
 
   def photo_notification(param_photo, param_sender, param_receiver)
     @sender = param_sender
     @receiver = param_receiver
     @photo = param_photo
-    mail(:to => param_receiver.email, :subject => "Photo Notification")
+    mail(:to => named_email(param_receiver), :subject => "Photo Notification")
   end
 
   def message_notification(param_msg, param_sender, param_receiver)
     @sender = param_sender
     @receiver = param_receiver
     @msg = param_msg
-    mail(:to => param_receiver.email, :subject => "Message Notification")
+    mail(:to => named_email(param_receiver), :subject => "Message Notification")
   end
 
   def comment_notification(users, param_comment)
     @comment = param_comment
     mail(:bcc => users.map{|u| u.email}, :subject => "Comment Notification")
+  end
+
+  private
+  def named_email(user) 
+    "#{user.name} <#{user.email}>" 
   end
 end
