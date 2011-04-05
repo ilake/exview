86743130cf6358889384f77c08e0af9f19e67f1d
ilake <lake.ilakela+github@gmail.com>
2011-04-05 13:10:24 +0800

    add user admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+User.create!(:login => APP_CONFIG["admin_name"], :email => APP_CONFIG["admin_email"],
+             :password => APP_CONFIG["admin_password"],
+             :password_confirmation => APP_CONFIG["admin_password"],
+             :receive_quota_now => 0, :send_quota_max => 0,
+             :country_name => "TW", :hometown_country_name => "TW",
+             :active => true)
