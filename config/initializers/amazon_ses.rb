9d8d5b157717ec1ac70bf93b7f38949be37408c9
ilake <lake.ilakela+github@gmail.com>
2011-04-23 08:58:15 +0800

    Still show the key in the file

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,4 +1,3 @@
-amazon_creds = YAML::load(open("#{Rails.root}/config/app_config.yml"))
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  :access_key_id     => amazon_creds['aws_access_key_id'],
-  :secret_access_key => amazon_creds['aws_secret_access_key']
+  :access_key_id     => 'AKIAJZLWPTDVMHBLG4EQ',
+  :secret_access_key => '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'
