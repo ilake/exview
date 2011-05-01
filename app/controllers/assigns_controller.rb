c1c38dda739be04879d375222a0f66af46a9be3a
ilake <lake.ilakela+github@gmail.com>
2011-05-01 22:08:46 +0800

    change the users show, edit,and  user not found design. Add some socail functions in user not found page Format the user memo content

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -3,11 +3,14 @@ class AssignsController < ApplicationController
 
   def create
     @user = current_user.find_one_friend
-
     if @user.is_a?(User)
       flash.now[:notice] = "Meet a new friend [ #{@user.name} ], you could share a photo to #{@user.name} now"
+      @receive_photos = @user.receive_photos.order("created_at DESC").include_receiver
+      @sent_photos = @user.sent_photos.order("created_at DESC").include_sender
+
       render :template => "users/show"
     else
+      @assigneds = current_user.assigns.unsent.unexpired.includes(:receiver)
       render :template => "users/not_found"
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,11 +40,40 @@ module ApplicationHelper
     link_to human_country_name(user.hometown_country_name), wikipedia_url(user.hometown_country_name), :target => '_blank'
   end
 
-  def format_content(content)
-    "Does not writing anything yet" if content.blank?
-  end
-
   def region_flag(region)
     raw "<span class='country #{region}'> <img title='' alt=''  src='http://static1.postcrossing.com/images/blank.gif'> </span>"
+  end
+
+  def facebook_like
+    raw '<iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.outcircle.com%2F&amp;send=true&amp;layout=box_count&amp;width=60&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font&amp => eight=65" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:120px; height:65px;" allowTransparency="true"></iframe>'
+  end
+
+  def facebook_share
+    raw '<a name="fb_share" type="button_count" href="http://www.facebook.com/sharer.php">Share</a><script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>'
+  end
+
+  def twitter_button
+    raw '<a href="http://twitter.com/share" class="twitter-share-button" data-count="vertical" data-via="weoutcircle">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>'
+  end
+
+  def facebook_fans_page
+    raw '<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FOutcircle%2F171655049556028&amp;width=292&amp;colorscheme=light&amp;show_faces=true&amp;stream=true&amp => eader=true&amp => eight=607" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:607px;" allowTransparency="true"></iframe>'
+  end
+
+  HTTP_REGEX = /((http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/[A-Za-z\.0-9_\+\?%\/=&\-]*)?)/ix
+
+  def full_format_content(content)
+    c = content
+    return 'Does not writing anything yet' if c.blank?
+
+    return c.gsub(/\r\n/, "\n").gsub(/\n/) { |e|
+      "<br />" unless $` =~/<br \/>$/
+    }.gsub(HTTP_REGEX) do  |e|
+      if $` =~/(href|src)=(?:"|')$/
+        e
+      else
+        "<a href='#{e}'>#{e}</a>"
+      end
+    end
   end
 end
