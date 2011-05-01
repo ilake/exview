f5fd8abbae366da7a1cd862207b031d1cd8128db
ilake <lake.ilakela+github@gmail.com>
2011-05-01 14:32:54 +0800

    check the return user type in assigns controller

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -5,9 +5,11 @@ class AssignsController < ApplicationController
     @user = current_user.find_one_friend
 
     if @user.is_a?(User)
-      flash.now[:notice] = "Meet a new friend #{@user.name}, you could share a photo to #{@user.name} now"
+      flash.now[:notice] = "Meet a new friend [ #{@user.name} ], you could share a photo to #{@user.name} now"
+      render :template => "users/show"
+    else
+      render :template => "users/not_found"
     end
-    render :template => "users/show"
   end
 
 end
