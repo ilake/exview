930b4112a5cc713ced01e9c9735ca0f4b5085c38
ilake <lake.ilakela+github@gmail.com>
2011-05-01 17:00:28 +0800

    fix n+1 query problem by add include

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @photos = Photo.order("created_at DESC").limit(20)
+    @photos = Photo.order("created_at DESC").limit(20).includes(:sender, :receiver)
     if current_user
       render :action => "user_index"
     end
