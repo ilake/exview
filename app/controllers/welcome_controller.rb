7af2116530f3010adae4a6b1f7f5825d51541683
ilake <lake.ilakela+github@gmail.com>
2011-05-01 14:25:13 +0800

    add a welcome page for login user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     @photos = Photo.order("created_at DESC").limit(20)
+    if current_user
+      render :action => "user_index"
+    end
   end
 
 end
